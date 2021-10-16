076788def8ef6645bcacab5639adfd54fd0648d4
Will Warner <47200097+cofad@users.noreply.github.com>
2021-10-16 13:14:44 -0500

    refactor into functions

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
-export function main() {
+export function main(): void {
+  addEventListener();
+}
+
+function addEventListner(): void {
   addEventListener("fetch", (event) => {
-    event.respondWith(
-      new Response("Test two", {
-        status: 200,
-        headers: {
-          server: "deploy",
-          "content-type": "text/plain",
-        },
-      })
-    );
+    event.respondWith(handleRequest(event.request));
+  });
+}
+
+function handleRequest(request: Request) {
+  return new Response(request.url, {
+    status: 200,
+    headers: {
+      server: "deploy",
+      "content-type": "text/plain",
+    },
   });
 }
