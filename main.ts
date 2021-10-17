892605966b119cb79b57c413dc6602922ce9133b
Will Warner <will.r.warner@gmail.com>
2021-10-17 07:19:30 -0500

    get data from npmjs

--- main.ts
+++ main.ts
@@ -27,9 +27,38 @@ async function handleFetchEvent(request: Request): Promise<Response> {
       },
     });
   } else {
-    return new Response("404: resource not found", {
-      status: 404,
-      headers: { "content-type": "text/plain" },
-    });
+    const pathnameArray = pathname.split("/");
+    const packageName = pathnameArray[1];
+    const version = pathnameArray[2];
+
+    const response = await fetch(
+      `https://registry.npmjs.org/${packageName}/${version}`,
+      {
+        headers: { accept: "application/json" },
+      }
+    );
+
+    if (response.ok) {
+      const { name, version, dependencies } = await response.json();
+
+      return new Response(
+        JSON.stringify({
+          name: name,
+          version: version,
+          dependencies: dependencies,
+        }),
+        {
+          status: 200,
+          headers: {
+            "content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+    } else {
+      return new Response("404: resource not found", {
+        status: 404,
+        headers: { "content-type": "text/plain" },
+      });
+    }
   }
 }
