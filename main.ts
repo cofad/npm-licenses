b157a80bf7129f9ebaa3121a82d3b4c827ab2d7e
Will Warner <will.r.warner@gmail.com>
2021-10-17 06:45:36 -0500

    use FetchEvent and handle routes

--- main.ts
+++ main.ts
@@ -1,19 +1,35 @@
+import { FetchEvent } from "./fetch-event.ts";
+
 export function main(): void {
-  addFetchEventListener();
+  addFetchEventListener(handleFetchEvent);
 }
 
-function addFetchEventListener(): void {
-  addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event.request));
+function addFetchEventListener(
+  callback: (request: Request) => Promise<Response>
+): void {
+  // @ts-ignore: Argument of type '(event: FetchEvent) => void' is not assignable to parameter of type 'EventListenerOrEventListenerObject | null'.
+  addEventListener("fetch", (event: FetchEvent) => {
+    event.respondWith(callback(event.request));
   });
 }
 
-function handleRequest(request: Request) {
-  return new Response(request.url, {
-    status: 200,
-    headers: {
-      server: "deploy",
-      "content-type": "text/plain",
-    },
-  });
+async function handleFetchEvent(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url);
+
+  if (pathname === "/" || pathname === "/home") {
+    const homeFile = await Deno.readFile("./home.html");
+
+    return new Response(homeFile, {
+      status: 200,
+      headers: {
+        server: "deploy",
+        "content-type": "text/html; charset=UTF-8",
+      },
+    });
+  } else {
+    return new Response("404: resource not found", {
+      status: 404,
+      headers: { "content-type": "text/plain" },
+    });
+  }
 }
